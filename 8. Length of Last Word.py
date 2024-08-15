cce535b74aaa81ffe1174fdadd76f9e713f67798
jonty <jontybeglin@gmail.com>
2024-08-15 23:55:19 +0100

    Added a less cheaty way of no8

--- 8. Length of Last Word.py
+++ 8. Length of Last Word.py
@@ -7,3 +7,25 @@ class Solution(object):
         """
         return len(s.split()[-1])
 
+# more complicated way
+
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        s = s[::-1]
+        chars = False
+        length = 0
+        for i in range(len(s)):
+            if s[i] == " " and chars:
+                return length
+            if chars:
+                length += 1
+            if s[i] != " " and not chars:
+                chars = True
+                length += 1
+        return length
+            
+                
