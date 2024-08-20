f9eaeb9c57ec5222257361759766224b6fc27a2b
jonty <jontybeglin@gmail.com>
2024-08-20 23:59:06 +0100

    fixed solution 67

--- #67. Add Binary.py
+++ #67. Add Binary.py
@@ -1,6 +1,5 @@
 
-# very bad solution
-
+# Effective solution however memory intensive and a lot of code used
 class Solution(object):
     def addBinary(self, a, b):
         """
@@ -12,31 +11,36 @@ class Solution(object):
         ans = ""
         if len(a) < len(b):
             b, a = a, b
-        for i in range(len(a)-len(b)):
+        for i in range(len(a) - len(b)):
             b = "0" + b
-        for i in range(len(a)-1, -1, -1):
+        for i in range(len(a) - 1, -1, -1):
             if a[i] == "1" and b[i] == "1" and carry == "0":
                 carry = "1"
                 ans = "0" + ans
-            if a[i] == "1" and b[i] == "1" and carry == "1":
+            elif a[i] == "1" and b[i] == "1" and carry == "1":
                 carry = "1"
                 ans = "1" + ans
-            if a[i] == "1" and b[i] == "0" and carry == "0":
+            elif a[i] == "1" and b[i] == "0" and carry == "0":
                 carry = "0"
                 ans = "1" + ans
-            if a[i] == "1" and b[i] == "0" and carry == "1":
+            elif a[i] == "1" and b[i] == "0" and carry == "1":
                 carry = "1"
                 ans = "0" + ans
-            if a[i] == "0" and b[i] == "1" and carry == "0":
+            elif a[i] == "0" and b[i] == "1" and carry == "0":
                 carry = "0"
                 ans = "1" + ans
-            if a[i] == "0" and b[i] == "1" and carry == "1":
+            elif a[i] == "0" and b[i] == "1" and carry == "1":
                 carry = "1"
                 ans = "0" + ans
-            if a[i] == "0" and b[i] == "0" and carry == "0":
+            elif a[i] == "0" and b[i] == "0" and carry == "0":
                 carry = "0"
                 ans = "0" + ans
-            if a[i] == "0" and b[i] == "0" and carry == "1":
+            elif a[i] == "0" and b[i] == "0" and carry == "1":
                 carry = "0"
-                ans = "0" + ans
+                ans = "1" + ans
+        
+        # Handle the case where there's still a carry after the loop
+        if carry == "1":
+            ans = "1" + ans
+        
         return ans
