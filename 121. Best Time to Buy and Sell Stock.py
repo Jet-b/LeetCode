1dfbd0f200ea9c60c776ae058994f9485084f727
Jonty Beglin <jontybeglin@gmail.com>
2024-08-31 23:53:44 +0100

    better solution

--- 121. Best Time to Buy and Sell Stock.py
+++ 121. Best Time to Buy and Sell Stock.py
@@ -13,3 +13,19 @@ class Solution(object):
                 if val > maxVal:
                     maxVal = val
         return maxVal
+
+# O(n) solution
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        maxProfit = 0
+        buy = prices[0]
+        for sell in prices[0:]:
+            if sell >= buy:
+                maxProfit = max(maxProfit, sell - buy)
+            else:
+                buy = sell
+        return maxProfit
