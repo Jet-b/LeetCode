f37b4cdf929831a7d8ed84ac1c9c10b9ca69e4fb
Jonty Beglin <jontybeglin@gmail.com>
2024-12-11 15:35:24 +0000

    added better solution to Q1

--- 1. Two Sum.py
+++ 1. Two Sum.py
@@ -7,3 +7,12 @@ class Solution:
             elif remaining in nums[i+1::]:
                 return [i, nums[i+1::].index(remaining) + i + 1]
 
+
+# Superior solution
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        lookup = {}
+        for i in range(len(nums)):
+            if nums[i] in lookup.keys():
+                return i, lookup[nums[i]]
+            lookup[target-nums[i]] = i
